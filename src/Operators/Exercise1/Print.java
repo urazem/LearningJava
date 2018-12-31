85005e064c3f7e7680db8380f7b0b13dec248700
Emine_ <urazalieva.e.i.2.16@gmail.com>
2018-12-31 10:09:41 +0300

    Added exercise solutions with 3 modules

---
+++ src/Operators/Exercise1/Print.java
@@ -0,0 +1,10 @@
+package Operators.Exercise1;
+
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("short  form of print: Hello it's me)");
+        System.out.println("\nlong  form of print: Hello it's me)");
+
+    }
+}
---
+++ src/Operators/Exercise13/BinaryForm.java
@@ -0,0 +1,19 @@
+package Operators.Exercise13;
+/*char values in binary form*/
+public class BinaryForm {
+    public static void showBinaryForm(char character) {
+        System.out.println(character + " = " + Integer.toBinaryString(character));
+    }
+    public static void main(String[] args) {
+        showBinaryForm('A');
+        showBinaryForm('a');
+        showBinaryForm('p');
+        showBinaryForm('U');
+    }
+
+}
+/*Output
+* A = 1000001
+* a = 1100001
+* p = 1110000
+* U = 1010101*/
